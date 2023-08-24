51d0ff77337c58a6e976ef2af0ea8653199d44df
NDOH NDOH Jules Moise Yannick <ndohmoise@gmail.com>
2023-08-24 20:41:13 +0200

    mise en place du header

---
+++ portefolio/src/components/home/HeaderSocials.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='home__socials'>
+        <a href="www.instagram.com" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+
+        <a href="www.facebook.com" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-facebook"></i>
+        </a>
+
+        <a href="www.twitter.com" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-twitter"></i>
+        </a>
+
+        <a href="www.linkedin.com" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-linkedin"></i>
+        </a>
+
+        <a href="youtube" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-youtube"></i>
+        </a>
+
+        <a href="www.behance.com" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-behance"></i>
+        </a>
+
+        <a href="www.pinterest.com" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-pinterest"></i>
+        </a>
+    </div>
+  )
+}
+
+export default HeaderSocials
--- portefolio/src/components/home/home.jsx
+++ portefolio/src/components/home/home.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import "./home.css";
+import Me from "../../assets/images/avatar-1.svg"
+import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
+import Shapes from './Shapes';
 
 const home = () => {
   return (
-    <div>
-      home
-    </div>
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={Me} alt="" className="home__img" />
+        <h1 className="home__name">Ndohvich</h1>
+        <span className="home__education">I'm a FullStack Developper And Data Science</span>
+
+        <HeaderSocials />
+
+        <a href="#contact" className="btn">Hire Me</a>
+
+        <ScrollDown />
+
+      </div>
+
+      <Shapes />
+
+    </section>
   )
 }
 
