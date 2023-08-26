d30730b92da5b0dba287d2b07ef704a37b0b1556
NDOH NDOH Jules Moise Yannick <ndohmoise@gmail.com>
2023-08-26 18:10:35 +0200

    Finition sur la partie expérience

--- portefolio/src/components/portfolio/Portfolio.jsx
+++ portefolio/src/components/portfolio/Portfolio.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import "./portfolio.css";
 function Portfolio() {
   return (
-    <div>
-      Portfolio
-    </div>
+    <section className="work container section" id="work">
+      <h2 className="section__title">Recent Works</h2>
+
+      <div className="work__filters">
+        <span className="work__item">Everything</span>
+        <span className="work__item">Creative</span>
+        <span className="work__item">Art</span>
+        <span className="work__item">Design</span>
+        <span className="work__item">Branding</span>
+      </div>
+
+      <div className="work__container grid"></div>
+    </section>
   )
 }
 
--- portefolio/src/components/resume/Resume.jsx
+++ portefolio/src/components/resume/Resume.jsx
@@ -1,11 +1,48 @@
+/* eslint-disable array-callback-return */
 import React from 'react'
 import "./resume.css"; 
+import Data from "./Data";
+import Card from "./Card";
 
 function Resume() {
   return (
-    <div>
-      Resume
-    </div>
+    <section className="resume container section" id="resume">
+      <h2 className="section__title">Experience</h2>
+        
+        <div className="resume__container grid">
+          <div className="timeline grid">
+            {Data.map((val, id) => {
+              if(val.category === "education") {
+                return (
+                  <Card 
+                    key={id}
+                    icon={val.icon}
+                    title={val.title}
+                    year={val.year}
+                    desc={val.desc} 
+                  />
+                );
+              }
+            })}
+          </div>
+
+          <div className="timeline grid">
+            {Data.map((val, index) => {
+              if(val.category === "experience") {
+                return (
+                  <Card 
+                    key={index}
+                    icon={val.icon}
+                    title={val.title}
+                    year={val.year}
+                    desc={val.desc} 
+                  />
+                );
+              }
+            })}
+          </div>
+        </div>
+    </section>
   )
 }
 
